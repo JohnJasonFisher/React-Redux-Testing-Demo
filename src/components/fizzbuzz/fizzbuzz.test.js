1002c45405bc211207db085d08d29de3ab199e6f
John Jason Fisher <jjfisher91@gmail.com>
2019-02-20 22:51:21 -0600

    cleans formatting

--- src/components/fizzbuzz/fizzbuzz.test.js
+++ src/components/fizzbuzz/fizzbuzz.test.js
@@ -4,20 +4,20 @@ describe('fizzbuzz', () => {
 	it('given 1 returns 1', () => {
 		const value = fizzbuzz(1)
 		expect(value).toBe(1)
-	});
+	})
 
 	it('given 3 returns fizz', () => {
 		const value = fizzbuzz(3)
 		expect(value).toBe('fizz')
-	});
+	})
 
 	it('given 5 returns buzz', () => {
 		const value = fizzbuzz(5)
 		expect(value).toBe('buzz')
-	});
+	})
 
 	it('given 15 returns fizzbuzz', () => {
 		const value = fizzbuzz(15)
 		expect(value).toBe('fizzbuzz')
-	});
-});
+	})
+})
